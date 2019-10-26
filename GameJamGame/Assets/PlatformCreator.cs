4bb2496d7f31f0f2dbcdd29abcdc7369c3c6da26
Rene <Rene.Gnahs@gmx.de>
2019-10-26 11:07:50 +0200

    add plasttrform creator, add follow camera script

--- GameJamGame/Assets/PlatformCreator.cs
+++ GameJamGame/Assets/PlatformCreator.cs
@@ -1,18 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformCreator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	public GameObject BottumPlane;
+	public GameObject SpawnObjects;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	private void Start()
+	{
+		//StartCoroutine(SpawnRoutine());
+	}
+
+	IEnumerator SpawnRoutine()
+	{
+		yield return new WaitForSeconds(0.2f);
+
+		float dis;
+
+		while (true)
+		{
+			dis = (transform.position - BottumPlane.transform.position).magnitude;
+
+			if (dis < 10)
+			{
+				SpawnObject();
+				transform.position += new Vector3(1,0,0);
+			}
+			yield return new WaitForSeconds(0.2f);
+		}
+
+	}
+
+	[SerializeField]
+	private float _radius = 5;
+
+	protected Vector3 CalculatePossibleSpawnPoints()
+	{
+		Vector3 randomPoint = UnityEngine.Random.insideUnitSphere * _radius;
+		randomPoint.z = 0;
+		randomPoint.y = 0;
+
+		Vector3 spawnPoints  = this.transform.position + randomPoint;
+
+		return spawnPoints;
+	}
+
+	void SpawnObject()
+	{
+		Instantiate(SpawnObjects, CalculatePossibleSpawnPoints(), Quaternion.identity);
+	}
+
+
 }
